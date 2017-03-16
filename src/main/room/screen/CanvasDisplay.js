32905f46042c8e59c5db41c6bde1c3345cf1733e
ppeccin <ppeccin@gmail.com>
2017-03-16 19:18:31 -0300

    WIP: release 4 Icons updated with 10% larger logo

--- src/main/room/screen/CanvasDisplay.js
+++ src/main/room/screen/CanvasDisplay.js
@@ -1,6 +1,5 @@
 // Copyright 2015 by Paulo Augusto Peccin. See license.txt distributed with this file.
 
-// TODO Remove unstable UNICODE chars (Paste icon, Arrows in Settings)
 // TODO Remove "Center" rounding problems as possible. Main screen element centering still remaining
 // TODO Possible to use hotkeys and bypass logo messages
 
